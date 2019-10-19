03eeb15202a0f3bc0e9557bee1d340ea96c57590
nakata yuki <nasutaro211@gmail.com>
2019-10-20 07:12:05 +0900

    domain一覧を返すエンドポイントの作成

--- rails/app/controllers/forcom/domains_controller.rb
+++ rails/app/controllers/forcom/domains_controller.rb
@@ -1,4 +1,7 @@
 class Forcom::DomainsController < ApplicationController
   def index
+    # 将来的には会社ごとに返す
+    domains = Domain.all
+    render json: domains, state: 'SUCCESS', message: 'successfully get domains'
   end
 end
