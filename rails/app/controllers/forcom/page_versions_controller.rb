b3d4aa4b0aab468bf0e29e44008f21fe0f28fdd6
nakata yuki <nasutaro211@gmail.com>
2019-10-20 07:43:45 +0900

    pageからpage_versionsを返すエンドポイントを作成

--- rails/app/controllers/forcom/page_versions_controller.rb
+++ rails/app/controllers/forcom/page_versions_controller.rb
@@ -1,7 +1,9 @@
 class Forcom::PageVersionsController < ApplicationController
-  
+
   def index
-    
+    page = Page.find(params[:page_id])
+    page_versions = page.page_versions
+    render json: {page_versions: page_versions, page: page}, state: 'SUCCESS', message: 'successfully get page_versions'
   end
-  
+
 end
