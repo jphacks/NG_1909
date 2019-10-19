d392558b74aec59adc30fd5023426762541df2ce
nakata yuki <nasutaro211@gmail.com>
2019-10-20 07:55:10 +0900

    domainごとのpageviewを持ってこれるようにした

--- rails/app/javascript/views/transition_view.vue
+++ rails/app/javascript/views/transition_view.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     {{ domain_id }}
+    {{ page_views }}
     <p v-text="content"></p>
     <div class="flex-row">
         <div class="flex-column" v-for="items in itemss">
@@ -31,6 +32,8 @@
 
 <script type="text/javascript">
 
+import axios from 'axios'
+
 export default {
   props: [
     "domain_id"
@@ -39,8 +42,18 @@ export default {
     return {
       content:'View transition here',
       item:'',
-      itemss:[[{path: "aa", height: 50}, {path: "bb", height: 10}, {path: "cc", height: 10}], [{path: "dd", height: 10}, {path: "ee", height: 15}, {path: "vv", height: 10}],[{path: "dd", height: 10}, {path: "ee", height: 10}, {path: "vv", height: 0}],[{path: "tt", height: 10}, {path: "pp", height: 0}], [{path: "rr", height: 0}]]
+      itemss:[[{path: "aa", height: 50}, {path: "bb", height: 10}, {path: "cc", height: 10}], [{path: "dd", height: 10}, {path: "ee", height: 15}, {path: "vv", height: 10}],[{path: "dd", height: 10}, {path: "ee", height: 10}, {path: "vv", height: 0}],[{path: "tt", height: 10}, {path: "pp", height: 0}], [{path: "rr", height: 0}]],
+      page_views: []
     }
+  },
+  created: function(){
+    axios.get(`/forcom/domains/${this.domain_id}/page_views`).then(resp=>{
+      console.log(resp.data);
+      this.page_views = resp.data.page_views
+    })
+    // axios.get(`/forcom/pages/4/page_versions`).then(resp=>{
+    //   console.log(resp);
+    // })
   }
 }
 
