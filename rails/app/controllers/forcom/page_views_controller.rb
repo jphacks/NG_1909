475ffc7c5f08a696ab07a1a58d5e35bafd3f354d
nakata yuki <nasutaro211@gmail.com>
2019-10-20 07:34:22 +0900

    domainからpage_viewsを返すエンドポイントの作成

--- rails/app/controllers/forcom/page_views_controller.rb
+++ rails/app/controllers/forcom/page_views_controller.rb
@@ -1,11 +1,12 @@
 class Forcom::PageViewsController < ApplicationController
-  
+
   def index_by_domain
-    
+    page_views = PageView.by_domain_id(params[:domain_id])
+    render json: page_views, state: 'SUCCESS', message: 'successfully get page_views'
   end
-  
+
   def index_by_page_version
     
   end
-  
+
 end
--- rails/app/models/page_view.rb
+++ rails/app/models/page_view.rb
@@ -1,4 +1,12 @@
 class PageView < ApplicationRecord
   belongs_to :session
   belongs_to :page_version
+
+  scope :by_domain_id, -> (domain_id) {
+    joins(
+      page_version: [
+        page: :domain
+      ]
+    ).merge(Domain.where(id: domain_id))
+  }
 end
