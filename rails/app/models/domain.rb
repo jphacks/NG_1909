e2133f34e0eeabfe1b135beff80657309ed520ed
nakata yuki <nasutaro211@gmail.com>
2019-10-19 22:50:01 +0900

    domainのfind_or_create_by_all_domainを作成した

--- rails/app/models/domain.rb
+++ rails/app/models/domain.rb
@@ -1,2 +1,17 @@
 class Domain < ApplicationRecord
+
+  before_save :set_root_domain
+  def set_root_domain
+    splited = self.all_domain.split(".")
+    self.root_domain = splited[-2] + "." + splited[-1]
+  end
+
+  def self.find_or_create_by_all_domain(all_domain)
+    # あったら
+    domain = Domain.find_by(all_domain: all_domain)
+    return domain if domain.present?
+    #  なかったら
+    domain = Domain.create(all_domain: all_domain)
+    return domain
+  end
 end
