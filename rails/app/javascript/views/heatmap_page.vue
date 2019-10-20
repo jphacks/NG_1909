cbcd89f0ff8ebabd6a820a102a8c0de58feabaa5
空 <sora@sora-no-MacBook-Pro.local>
2019-10-20 11:12:21 +0900

    ヒートマップを生成できるようにした

--- rails/app/javascript/views/heatmap_page.vue
+++ rails/app/javascript/views/heatmap_page.vue
@@ -2,10 +2,10 @@
   <div id="app">
     {{ page_id }}
     {{ page_versions }}
-    {{ page_views }}
+    <!-- {{ page_views }} -->
     <p v-text="content"></p>
     <v-select :items="page_versions" item-text="id" item-value="id" @change="changeVersion" label="バージョン"></v-select>
-    <img src="/captures/sample-lp2.jpg" alt="" width="70%">
+    <div id='heatMap'></div>
   </div>
 </template>
 
@@ -20,10 +20,22 @@
     flex-direction: column;
 }
 
+#heatMap {
+
+width:1000px; height:1000px;
+border:1px solid; background-color:yellow;
+overflow:auto;
+resize:both;
+
+background-image:url(/captures/sample-lp2.jpg);
+background-size: cover;
+}
+
 </style>
 
 <script type="text/javascript">
 import axios from 'axios'
+import heatmapjs from 'heatmapjs'
 
 export default {
   props: [
@@ -31,7 +43,7 @@ export default {
   ],
   data: function(){
     return {
-      content:'This is heatmap page',
+      content:'This is heatmapjs page',
       item:'',
       itemss:[[{path: "aa", height: 50}, {path: "bb", height: 10}, {path: "cc", height: 10}], [{path: "dd", height: 10}, {path: "ee", height: 15}, {path: "vv", height: 10}],[{path: "dd", height: 10}, {path: "ee", height: 10}, {path: "vv", height: 0}],[{path: "tt", height: 10}, {path: "pp", height: 10}], [{path: "rr", height: 10}]],
       page_versions: [],
@@ -43,6 +55,19 @@ export default {
       this.page_versions = resp.data.page_versions
     })
   },
+  computed: {
+    dots: function(){
+      var data = []
+      this.page_views.forEach(function(page_view){
+        page_view.gazes.forEach(element => {
+          var x=Math.round(element.x*1000);
+          var y=Math.round(element.y*1000);
+          data.push({x:x,y:y,value:1});
+        });
+      })
+      return data
+    }
+  },
   methods: {
     changeVersion: function (id) {
       console.log(id);
@@ -50,6 +75,17 @@ export default {
         console.log(resp);
         this.page_views = resp.data.page_views
       })
+    }
+  },
+  watch: {
+    "dots": function(value){
+      var heatmap = heatmapjs.create({
+        container: document.getElementById("heatMap")
+      })
+      heatmap.setData({
+        max: 1,
+        data: value
+      });
     }
   }
 }
